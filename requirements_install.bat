08252da641350bf858d5a3b29e961f4da6615c73
admin <023>
2023-09-13 17:18:50 +0300

    add requirements_install.bat

---
+++ requirements_install.bat
@@ -0,0 +1,14 @@
+where pip
+where python
+rem pause
+
+rem if file is in upper directory level!
+rem cd ..
+
+pip install --upgrade pip
+pip install -r requirements.txt
+
+rem C:\Python38\Scripts\pip install --upgrade pip
+rem C:\Python38\Scripts\pip install -r requirements.txt
+
+pause
