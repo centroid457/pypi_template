adc7147acb550dbdc02d09b975e2209750fb8006
admin <023>
2023-09-18 17:59:06 +0300

    add print(f"exit in [{i}] seconds")

--- _5=update_pip.py
+++ _5=update_pip.py
@@ -1,6 +1,15 @@
 import pip
 
-pip.main(["install", "--upgrade", "pip"])
 pip.main(["install", "--upgrade", "NEW_PROJECT____"])
 
-input("press Enter to close")
+
+# EXIT PAUSE ==========================================================================================================
+# input("press Enter to close")
+
+import time
+for i in range(3, 0, -1):
+    print(f"exit in [{i}] seconds")
+    time.sleep(1)
+
+
+# =====================================================================================================================
