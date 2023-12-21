60559402a10a4cec15f1070f3be2a99e81d3c77f
Andrei Starichenko <a.starichenko@element-t.ru>
2023-12-21 10:33:32 +0300

    separate readme-update into README.py

--- PROJECT.py
+++ PROJECT.py
@@ -8,15 +8,28 @@ class PROJECT:
     AUTHOR_EMAIL: str = "centroid@mail.ru"
     AUTHOR_HOMEPAGE: str = "https://github.com/centroid457/"
 
-    # CHANGE -----------------------------------------------
-    VERSION: str = "0.0.1"
+    # ------------------------------------------------------
+    NAME_INSTALL: str = "prj-name"
+    NAME_IMPORT: str = "prj_name"
+
+    DESCRIPTION: str = "description"
     KEYWORDS: List[str] = [
         "kw1",
     ]
+    WISHES: List[str] = [
+        "add ..."
+    ]
 
-    NAME_INSTALL: str = "prj-name"
-    NAME_IMPORT: str = "prj_name"
-    DESCRIPTION: str = "description"
+    # CHANGE -----------------------------------------------
+    VERSION: str = "0.0.1"
+    NEWS: List[str] = [
+        "add ..."
+    ]
+
+
+# =====================================================================================================================
+if __name__ == '__main__':
+    pass
 
 
 # =====================================================================================================================
