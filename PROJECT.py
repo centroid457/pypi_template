9b59d9f4dad45c7c850c2b5ff568613dc609574a
Andrei Starichenko <centroid@mail.ru>
2024-06-20 16:08:20 +0300

    [auxRelease/readme] add BADGES block

--- PROJECT.py
+++ PROJECT.py
@@ -3,7 +3,8 @@ from _aux__release_files import release_files_update
 
 
 # =====================================================================================================================
-VERSION = (0, 0, 3)   # 1/deprecate _VERSION_TEMPLATE from PRJ object +2/place update_prj here in __main__ +3/separate finalize attrs
+# VERSION = (0, 0, 3)   # 1/deprecate _VERSION_TEMPLATE from PRJ object +2/place update_prj here in __main__ +3/separate finalize attrs
+VERSION = (0, 0, 4)     # add AUTHOR_NICKNAME_GITHUB for badges
 
 
 # =====================================================================================================================
@@ -12,6 +13,7 @@ class PROJECT:
     AUTHOR_NAME: str = "Andrei Starichenko"
     AUTHOR_EMAIL: str = "centroid@mail.ru"
     AUTHOR_HOMEPAGE: str = "https://github.com/centroid457/"
+    AUTHOR_NICKNAME_GITHUB: str = "centroid457"
 
     # PROJECT ----------------------------------------------
     NAME_IMPORT: str = "prj_name"
